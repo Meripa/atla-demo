c170dbeb7da522699ff2f251553de879b3ee4321
Meris <19meris@gmail.com>
2026-04-15 18:39:56 +0300

    lil changes

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,42 +1,29 @@
-import { Outlet, useLocation } from "react-router-dom"
-import { AnimatePresence, motion } from "framer-motion"
+import { Outlet, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-import ScrollToTop from "../components/ScrollToTop"
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const MainLayout = () => {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
     <div className="flex flex-col min-h-screen">
       <ScrollToTop />
       <Navbar />
 
-      <div className="flex-1 pt-24">
-        <AnimatePresence 
-          mode="wait"           // "wait" annab puhtama transitioni
-          initial={false}
-        >
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, y: 20 }}     // algne seisund
-            animate={{ opacity: 1, y: 0 }}      // lõplik seisund
-            exit={{ opacity: 0, y: -10 }}       // väljumine
-            transition={{ 
-              duration: 0.4, 
-              ease: "easeInOut" 
-            }}
-            style={{ opacity: 1 }}              // väldib flashi
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
-      </div>
+      <main className="flex-1 pt-24">
+
+      
+      
+        <Outlet />
+
+      </main>
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- src/pages/TehasetuurPage.tsx
+++ src/pages/TehasetuurPage.tsx
@@ -1,9 +1,15 @@
 import Tehasetuur from '../components/Tehasetuur'
-
+import { motion } from "framer-motion"
 const TehasetuurPage = () => {
   return (
     <>
+      <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
     <Tehasetuur />
+    </motion.div>
     </>
   )
 }
