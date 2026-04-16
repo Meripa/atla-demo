d2749d6de7b92500093a01ee668cb70ea4086d60
Meris <19meris@gmail.com>
2026-04-16 14:06:45 +0300

    Fix for launching

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,11 @@
-import { Outlet, useLocation } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
+import { Outlet } from "react-router-dom";
+
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ScrollToTop from "../components/ScrollToTop";
 
 const MainLayout = () => {
-  const location = useLocation();
 
   return (
     <div className="flex flex-col min-h-screen">
